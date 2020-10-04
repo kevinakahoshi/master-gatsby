53bba4adb8a06c8219f7f40251ee3fc615ee5352
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-10-03 21:26:38 -0700

    Completed module 5

---
+++ starter-files/gatsby/src/components/PizzaList.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'gatsby';
+import React from 'react';
+
+const SinglePizza = ({ pizza }) => (
+  <Link to={`/pizza/${pizza.slug.current}`}>
+    <h2>
+      <span className="mark">{pizza.name}</span>
+    </h2>
+    <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
+  </Link>
+);
+
+const PizzaList = ({ pizzas }) => (
+  <>
+    <h1>List of Pizzas</h1>
+    {pizzas.map((pizza) => (
+      <SinglePizza key={pizza.id} pizza={pizza} />
+    ))}
+  </>
+);
+
+export default PizzaList;
