526e068749e616a220d04b1a4992cfb14221791a
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-10-08 17:53:10 -0700

    Pulled in data from dynamic query

--- starter-files/gatsby/src/pages/index.jsx
+++ starter-files/gatsby/src/pages/index.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import CurrentlySlicing from '../components/CurrentlySlicing';
+import HotSlices from '../components/HotSlices';
 import SEO from '../components/SEO';
+import useLatestData from '../utils/useLatestData';
 
-const HomePage = () => (
-  <>
-    <SEO title="Hot Now!" />
-    <h1>Hey! I'm the home page</h1>
-  </>
-);
+const HomePage = () => {
+  const data = useLatestData();
+  const { sliceMasters, hotSlices } = data;
+
+  return (
+    <>
+      <SEO title="Hot Now!" />
+      <div className="center">
+        <h1>The Best Pizza Downtown!</h1>
+        <p>Open 11:00AM - 11:00PM every single day!</p>
+        <div>
+          <CurrentlySlicing sliceMasters={sliceMasters} />
+          <HotSlices hotSlices={hotSlices} />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default HomePage;
