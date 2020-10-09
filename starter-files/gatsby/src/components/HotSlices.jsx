e1a16d3083f6435dabeebd3e5023e956119182f7
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-10-09 12:46:27 -0700

    Completed module 12

--- starter-files/gatsby/src/components/HotSlices.jsx
+++ starter-files/gatsby/src/components/HotSlices.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import LoadingGrid from './LoadingGrid';
+import ItemGrid from './ItemGrid';
 
 const HotSlices = ({ hotSlices }) => (
   <div>
+    <h2 className="center">
+      <span className="mark tilt">Slices On</span>
+    </h2>
+    <p>Come on by, buy the slice!</p>
     {!hotSlices && <LoadingGrid count={4} />}
     {hotSlices && !hotSlices?.length && <p>We got nothing in the case!</p>}
+    {hotSlices?.length && <ItemGrid items={hotSlices} />}
   </div>
 );
 
